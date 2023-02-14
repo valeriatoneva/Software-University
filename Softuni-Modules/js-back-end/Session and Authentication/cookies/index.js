3ce782a6bd9298695d277f0c6c7a665d8b18bae5
valeriatoneva <106249179+valeriatoneva@users.noreply.github.com>
2023-02-14 13:44:10 +0200

    save

--- Softuni-Modules/js-back-end/Session and Authentication/cookies/index.js
+++ Softuni-Modules/js-back-end/Session and Authentication/cookies/index.js
@@ -1,21 +1,26 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const cookieParser = require("cookie-parser"); // func
-const app = express();
 
+const dataService = require('./dataService')
+const app = express();
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // creating a route
     res.send(`
     <h1> Hello </h1>
     <p><a href="/login">Login</a></p>
+    <p><a href="/register">Register</a></p>
     <p><a href="/profile">Profile</a></p>
+    <p><a href="/logout">Logout</a></p>
     `
     )
 })
 
 app.get("/login", (req, res) => {
   res.send(`
+    <h1> Login </h1>
     <form method="POST">
     <label for="username">Username</label>
     <input type="text" id="username" name="username"/>
@@ -31,32 +36,63 @@ app.get("/login", (req, res) => {
 app.post('/login', (req, res) => {
 const { username, password } = req.body
 
-if(username == 'Ivan' && password == 'peti'){
-    const authData = {
-        username: 'Ivan',
-
-    }
- res.cookie('auth', JSON.stringify(authData))
- return res.redirect('/')
+try{
+   const user = await dataService.loginUser(username, password)
+   const authData = {
+    username: user.username,
 }
 
-res.status(401).end()
+res.cookie('auth', JSON.stringify(authData))
+req.session.username = user.username;
+req.session.privateInfo = user.password; // the hash
+return res.redirect('/')
+
+} catch(err){
+    console.log(err)
+    res.status(401).end()
+}
+})
+
+app.get('/register', async (req, res) => {
+res.send(`
+<h1> Register </h1>
+<form method="POST">
+<label for="username">Username</label>
+<input type="text" id="username" name="username"/>
+
+<label for="password">password</label>
+<input type="password" id="password" name="password"/>
+
+<input type="submit" value="register"/>
+</form>
+`)
+})
+
+// doing smth with that get response
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    await dataService.registerUser(username, password);
+    res.redirect('/login')
 })
 
 app.get('/profile', (req, res) => {
     const authData = req.cookies['auth'];
 
-    if(!authData){
-        return res.status(401).end()
+    if(!authData){ // if the user in unauthenticated
+        return res.status(401).end() // ends the response
     }
 
-    const {username} = JSON.parse(authData);
-
+    const {username} = JSON.parse(authData); // extracting the username from the cookie
     console.log(username);
 
     res.send(`
     <h2> Hello - ${username}</h2>
     `)
+})
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('auth')
+    res.redirect('/')
 })
 
 app.listen(5000, ()=> console.log('Server is listening on port 5000...'))
