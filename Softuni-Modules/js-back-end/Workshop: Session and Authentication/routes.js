41d73c446a2ef5886f35c5674b13684389bf4971
valeriatoneva <106249179+valeriatoneva@users.noreply.github.com>
2023-02-15 16:19:30 +0200

    save

---
+++ Softuni-Modules/js-back-end/Workshop: Session and Authentication/routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+// taking all controllers
+const cubeController = require('./controllers/cubeController');
+const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
+
+// for the given method and path, use the current action 
+router.get('/', homeController.getHomePage);
+router.get('/about', homeController.getAboutPage);
+router.get('/404', homeController.getErrorPage);
+
+router.get('/cubes/create', cubeController.getCreateCube);
+router.post('/cubes/create', cubeController.postCreateCube);
+router.get('/cubes/:cubeId/details', cubeController.getDetails);
+router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory);
+router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
+
+router.use('/accessories', accessoryController);
+
+module.exports = router;
