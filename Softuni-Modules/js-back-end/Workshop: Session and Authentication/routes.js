d697fad4191be0c15cfbd11e7fbb3d620dcdd1c5
valeriatoneva <106249179+valeriatoneva@users.noreply.github.com>
2023-02-15 16:59:23 +0200

    added login and register pages

--- Softuni-Modules/js-back-end/Workshop: Session and Authentication/routes.js
+++ Softuni-Modules/js-back-end/Workshop: Session and Authentication/routes.js
@@ -4,11 +4,14 @@ const router = require('express').Router();
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 // for the given method and path, use the an action 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 router.get('/404', homeController.getErrorPage);
+
+router.use('/', authController)
 
 router.get('/cubes/create', cubeController.getCreateCube);
 router.post('/cubes/create', cubeController.postCreateCube);
